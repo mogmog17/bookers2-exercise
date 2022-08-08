49e120d1d5f99010fbc4453b79ceace3a0e2fa4a
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-08-08 11:53:05 +0000

    [Update]復習2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,16 +7,37 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = @Book.user
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   def index
-    @users = @User.all
+    @book = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
