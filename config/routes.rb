47d44ca700c84383dc7712c0254533bb97333d5c
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-08-09 08:12:06 +0000

    [Update]復習4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   root to: 'homes#top'
-  get '/about' => 'homes#about'
-  resources :users
+  get 'home/about' => "homes#about", as:'about'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
