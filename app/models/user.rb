4c79ec9fa28225b58e5edca0f9860c91ac7a36ae
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-10-17 12:47:50 +0000

    [Add]フォロー、フォロワー機能のアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,16 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # フォローをした、されたの関係
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+
+  # フォロー、フォロワー一覧画面で使う
+
+  has_many :followings, through: :relationships, source: :following
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
 
@@ -19,5 +29,23 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  # フォローしたときの処理
+
+  def follow(user_id)
+    relationships.create(following_id: user_id)
+  end
+
+  # フォローを外すときの処理
+
+  def unfollow(user_id)
+    relationships.find_by(following_id: user_id).destroy
+  end
+
+  # フォローしているか判定
+
+  def following?(user)
+    followings.include?(user)
   end
 end
