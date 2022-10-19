34d8953864ba111370ff537389217531dab23d67
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-10-19 14:13:31 +0000

    [Add]フォローボタン、ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
 
   devise_scope :user do
@@ -9,7 +7,11 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about' => "homes#about", as:'about'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
